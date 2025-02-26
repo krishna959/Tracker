373a659dd9e8ecdd9175c000717e25569cce468e
Krishna Patel <139553378+krishna959@users.noreply.github.com>
2025-02-26 18:56:06 +0530

    Update urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('add-activity/', views.add_activity, name='add_activity'),
     path('add-meal/', views.add_meal, name='add_meal'),
     path("delete-activity/<int:activity_id>/", views.delete_activity, name="delete_activity"),
+    path("delete-meal/<int:meal_id>/", views.delete_meal, name="delete_meal"),
     # path('',)
 ]
 
